3d70202c15ba8cb3dbe5978f7c5b5162cfbb6105
DrAllStar16 <alexander.somich@gmail.com>
2020-07-10 16:50:14 -0700

    Finsished Algorithms and SearchingAndSortingAlgorithms

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -65,24 +65,49 @@ public class Algorithms {
 	}
 
 	public static List<String> sortDNA(List<String> unsortedSequences) {
-		while(unsortedSequences.get(0).length() > unsortedSequences.get(3).length()) {
-		if (unsortedSequences.get(0).length() > unsortedSequences.get(1).length()) {
-			unsortedSequences.set(0, unsortedSequences.get(1));
-			unsortedSequences.set(1, unsortedSequences.get(0));
-		}
-		if (unsortedSequences.get(1).length() > unsortedSequences.get(2).length()) {
-			unsortedSequences.set(1, unsortedSequences.get(2));
-			unsortedSequences.set(2, unsortedSequences.get(1));
-		}
-		if (unsortedSequences.get(2).length() > unsortedSequences.get(3).length()) {
-			unsortedSequences.set(2, unsortedSequences.get(3));
-			unsortedSequences.set(3, unsortedSequences.get(4));
-		}
-		if (unsortedSequences.get(3).length() > unsortedSequences.get(4).length()) {
-			unsortedSequences.set(3, unsortedSequences.get(4));
-			unsortedSequences.set(4, unsortedSequences.get(3));
-		} 
+
+		for (int i = 0; i < 5; i++) {
+			if (unsortedSequences.get(3).length() > unsortedSequences.get(4).length()) {
+				String first = unsortedSequences.get(3);
+				String second = unsortedSequences.get(4);
+				unsortedSequences.set(3, second);
+				unsortedSequences.set(4, first);
+			}
+
+			if (unsortedSequences.get(2).length() > unsortedSequences.get(3).length()) {
+				String first = unsortedSequences.get(2);
+				String second = unsortedSequences.get(3);
+				unsortedSequences.set(2, second);
+				unsortedSequences.set(3, first);
+			}
+
+			if (unsortedSequences.get(1).length() > unsortedSequences.get(2).length()) {
+				String first = unsortedSequences.get(1);
+				String second = unsortedSequences.get(2);
+				unsortedSequences.set(1, second);
+				unsortedSequences.set(2, first);
+			}
+
+			if (unsortedSequences.get(0).length() > unsortedSequences.get(1).length()) {
+				String first = unsortedSequences.get(0);
+				String second = unsortedSequences.get(1);
+				unsortedSequences.set(0, second);
+				unsortedSequences.set(1, first);
+			}
+
 		}
 		return unsortedSequences;
+	}
+
+	public static List<String> sortWords(List<String> words) {
+		for (int i = 0; i > words.size(); i++) {
+			if (words.get(i).compareTo(words.get(i + 1)) > 0) {
+				String first = words.get(i);
+				String second = words.get(i + 1);
+				words.set(i, second);
+				words.set(i + 1, first);
+			}
+		}
+		return words;
 	}
 }
